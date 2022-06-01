967a5ba1d779733e859caa969070d9e6edcfc20a
Diogo <2201719@my.ipleiria.pt>
2022-06-01 08:50:16 +0100

    Continue - Data.v1

--- src/main/java/modelo/Data.java
+++ src/main/java/modelo/Data.java
@@ -1,5 +1,7 @@
 package modelo;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
@@ -9,12 +11,36 @@ public class Data {
     private Calendar calendar;
 
     public Data(int dia,int mes,int ano){
-        this.calendar = new GregorianCalendar(ano, mes - 1 , dia);
+        this.calendar = new GregorianCalendar(ano,mes,dia);
     }
 
     public static Data parseData(String data){
         //TODO
 
-        return new Data(ano,mes,dia);
+        //String partes[] = data.split("/");
+
+        var formato = new SimpleDateFormat("dd/MM/yyyy");
+        formato.setLenient(false);
+        try {
+            var d = formato.parse(data);
+            var calendar= new GregorianCalendar();
+            calendar.setTime(d);
+            int dia = calendar.get(Calendar.DAY_OF_MONTH);
+            int mes = calendar.get(Calendar.MONTH) +1 ;
+            int ano = calendar.get(Calendar.YEAR);
+
+            return new Data(ano,mes,dia);
+        }catch (ParseException e){
+            return null;
+        }
+
+    }
+
+    @Override
+    public String toString() {
+        int dia = calendar.get(Calendar.DAY_OF_MONTH);
+        int mes = calendar.get(Calendar.MONTH) +1 ;
+        int ano = calendar.get(Calendar.YEAR);
+        return dia + "/" + mes + "/" +ano;
     }
 }
